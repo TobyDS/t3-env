e365d56e27a66ba72dd92b96a44d5e039adfa95d
Toby Dixon Smith <tobydixonsmith@gmail.com>
2024-12-14 19:50:14 +0000

    fix: Update examples/nextjs/next.config.js
    Current approach used deprecated default export and jiti function. Updated to new implementation.

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,10 +1,12 @@
-import createJiti from "jiti";
+import { createJiti } from "jiti";
 const jiti = createJiti(new URL(import.meta.url).pathname);
 
 // Import env here to validate during build. Using jiti we can import .ts files :)
-jiti("./app/env");
+await jiti("./app/env");
 
 /** @type {import('next').NextConfig} */
-export default {
-  eslint: { ignoreDuringBuilds: true },
+const nextConfig = {
+  /* config options here */
 };
+
+export default nextConfig;
